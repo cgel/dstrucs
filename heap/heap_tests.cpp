d1c306868a97c126536caebfd7f2aa2731ebf8e3
cgel <carlesgelada@hotmail.com>
2015-10-19 12:50:05 +0200

    new test system for heap

---
+++ heap/heap_tests.cpp
@@ -0,0 +1,19 @@
+#include "gtest/gtest.h"
+#include "heap.h"
+
+double root(double val) { return 1.0; }
+
+TEST(test_heap, heap) {
+  Heap<int> h;
+  h.insert(7);
+  h.insert(2);
+  EXPECT_EQ(7, h.top());
+  h.pop();
+  EXPECT_EQ(2, h.top());
+}
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
