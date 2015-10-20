5fa3c90fe8db82bf65fba0f1d2d4233d944c8d42
cgel <carlesgelada@hotmail.com>
2015-10-20 15:27:02 +0200

    Solved the bug that did not allow the tests to pass. Redesigned bubbleDown as recursive

--- heap/heap.h
+++ heap/heap.h
@@ -14,16 +14,16 @@ template <class T> struct Smaller {
 template <class T, class Compare = Greater<T> > class Heap {
 private:
   typedef T *iterator;
-  T *elem;   // a pointer to the first object
-  int space; // the index of the first free object
-  int last;  // the index of one passed the last object
+  T *buff;   // a pointer to the first object
+  int _empty; // the index of the first free object
+  int _last;  // the index of one passed the last object
 
   int capacity();
   void reallocate();
   inline int freeSpace();
   void swap(int, int);
 
-  bool bubbleDown(int &k);
+  void bubbleDown(int k);
   void bubbleUp(int k);
   Compare comp;
 
@@ -34,83 +34,70 @@ public:
   T &top();
   void pop();
   void insert(T);
+  int size();
 #ifdef __PC
   void print();
 #endif
-  int size();
 };
 
 template <class T, class Compare> Heap<T, Compare>::Heap() {
-  elem = new T[STD_BUFFER];
-  space = 0;
-  last = STD_BUFFER;
+  buff = new T[STD_BUFFER];
+  _empty = 0;
+  _last = STD_BUFFER;
 }
 
-template <class T, class Compare> Heap<T, Compare>::~Heap() { delete[] elem; }
+template <class T, class Compare> Heap<T, Compare>::~Heap() { delete[] buff; }
 
 template <class T, class Compare> inline int Heap<T, Compare>::freeSpace() {
-  return last - space;
+  return _last - _empty;
 }
 
 template <class T, class Compare> void Heap<T, Compare>::reallocate() {
   int s = size();
   int newSize = s * 2;
-  T *newElem = new T[newSize];
+  T *newBuff = new T[newSize];
   for (int i = 0; i != s; i++) {
-    newElem[i] = elem[i];
+    newBuff[i] = buff[i];
   }
-  delete[] elem;
-  elem = newElem;
-  space = s;
-  last = newSize;
+  delete[] buff;
+  buff = newBuff;
+  _empty = s;
+  _last = newSize;
 }
 
 template <class T, class Compare> void Heap<T, Compare>::swap(int i, int j) {
-  T temp = elem[i];
-  elem[i] = elem[j];
-  elem[j] = temp;
+  T temp = buff[i];
+  buff[i] = buff[j];
+  buff[j] = temp;
 }
 
-template <class T, class Compare> bool Heap<T, Compare>::bubbleDown(int &k) {
-  int lc = k * 2 + 1, rc = lc + 1;
-  // it is possible that some of the childs are outside of the allocated space
-
-  bool lb, rb;
-  lb = lc < last;
-  rb = rc < last;
-
-  if (lb && rb) {
-    if (comp(elem[k], elem[lc]) && comp(elem[k], elem[rc])) {
-      return true; // no need to bubble down
-    }
-
-    if (comp(elem[lc], elem[rc])) // left child is greater
+template <class T, class Compare> void Heap<T, Compare>::bubbleDown(int index) {
+  int left_child = index * 2 + 1, right_child = left_child + 1;
+  // it is possible that some of the child indexes are constructed or outside the buffer range 
+  bool bubble_left = left_child < _empty;
+  bool bubble_right = right_child < _empty;
+  if (bubble_left && bubble_right) {
+    if (comp(buff[left_child], buff[right_child])) // left child is greater
     {
-      swap(k, lc);
-      k = lc;
-      return false;
-    } else { // right child is greater
-      swap(k, rc);
-      k = rc;
-      return false;
+      swap(index, left_child);
+      bubbleDown(left_child);
+    } else if (comp(buff[right_child], buff[left_child])){ // left child is greater
+      swap(index, right_child);
+      bubbleDown(right_child);
     }
-  } else if (lb) {
-    if (comp(elem[lc], elem[k])) // left child is greater
+  } else if (bubble_left) {
+    if (comp(buff[left_child], buff[index])) // left child is greater
     {
-      swap(k, lc);
-      k = lc;
-      return false;
-    } else {
-      return true;
+      swap(index, left_child);
+      bubbleDown(left_child);
     }
-  } else {
-    return true;
   }
+  return;
 }
 
 template <class T, class Compare> void Heap<T, Compare>::bubbleUp(int index) {
   int parent_index = (index - 1) / 2; 
-  if (index == 0 || comp(elem[parent_index], elem[index])) {
+  if (index == 0 || comp(buff[parent_index], buff[index])) {
     return;
   }
   swap(index, parent_index);
@@ -119,35 +106,31 @@ template <class T, class Compare> void Heap<T, Compare>::bubbleUp(int index) {
 }
 
 template <class T, class Compare> void Heap<T, Compare>::pop() {
-  if (space == 0)
-    return; // so that if you pop an empty heap the program does not
-                 // crash. (but there is undefined behaviour)
-  --space;
-  elem[0] = elem[space];
-  int k = 0;
-  bool bubStop = false;
-  while (bubStop == false) {
-    bubStop = bubbleDown(k);
+  if (_empty != 0) {
+    --_empty;
+    buff[0] = buff[_empty];
+    bubbleDown(0);
   }
   return;
 }
 
 template <class T, class Compare> void Heap<T, Compare>::insert(T new_key) {
-  if (freeSpace() == 0)
-    reallocate();
-  elem[space] = new_key;
-  bubbleUp(space);
-  space++;
+  if (freeSpace() == 0) {
+    reallocate(); 
+  }
+  buff[_empty] = new_key;
+  bubbleUp(_empty);
+  _empty++;
 }
 
-template <class T, class Compare> T &Heap<T, Compare>::top() { return *elem; }
+template <class T, class Compare> T &Heap<T, Compare>::top() { return *buff; }
 
-template <class T, class Compare> int Heap<T, Compare>::size() { return space; }
+template <class T, class Compare> int Heap<T, Compare>::size() { return _empty; }
 
 #ifdef __PC
 template <class T, class Compare> void Heap<T, Compare>::print() {
-  for (int i = 0; i != space; i++)
-    std::cout << " " << elem[i];
+  for (int i = 0; i != _empty; i++)
+    std::cout << " " << buff[i];
 
   std::cout << std::endl;
 }
